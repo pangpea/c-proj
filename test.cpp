754169d4790d677e0001a8b86844d084637b0b8c
pangpea <2465280195@qq.com>
2017-09-27 11:43:17 +0800

    my first c++ project name:hello world!

---
+++ test.cpp
@@ -0,0 +1,20 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 2017Äê9ÔÂ24ÈÕ
+ *      Author: Administrator
+ */
+
+#include "test.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello, world!" << endl;
+  return 0;
+}
+
+
---
+++ test.h
@@ -0,0 +1,18 @@
+/*
+ * test.h
+ *
+ *  Created on: 2017Äê9ÔÂ24ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+#include <iostream>
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+#endif /* TEST_H_ */
